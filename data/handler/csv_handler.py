7b18b95cd87dc40346c372bbc1eae2433278c50f
tiborczuper <125367528+tiborczuper@users.noreply.github.com>
2025-01-12 21:57:52 +0100

    csv

---
+++ data/handler/csv_handler.py
@@ -0,0 +1,15 @@
+import os
+import csv
+from data import generator
+from model_dataclasses import School,Passport,Person
+#writing
+def write_schools(schools: list[School], file_path: str,heading:bool = True) -> None:
+    pass
+
+def write_passports(passports: list[Passport], file_path: str,heading:bool = True) -> None:
+    pass
+
+def write_persons(persons: list[Person], file_path: str,heading:bool = True) -> None:
+    pass
+
+#reading
---
+++ data/handler/xlsx_handler.py
@@ -0,0 +1,5 @@
+import os
+import csv
+from data import generator
+from model_dataclasses import School,Passport,Person
+

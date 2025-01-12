786a0dddae4b05ec2bddad50e89bee9e6d5c7d8d
tiborczuper <125367528+tiborczuper@users.noreply.github.com>
2025-01-12 21:16:40 +0100

    generator done

--- data/generator.py
+++ data/generator.py
@@ -1,49 +1,71 @@
 from faker import Faker
-from faker_vehicle import VehicleProvider
-from model_classes import Car, Company
+from faker_education import SchoolProvider
+from model_classes import School, Passport, Person
 import random
-
-def generate_cars(n: int, lang: str = "hu_HU", min_year: int = 1990, max_year: int = 2025) -> list[Car]:
-
-    fake_vehicle = Faker(lang)
-    fake_vehicle.add_provider(VehicleProvider)
+from dateutil.relativedelta import relativedelta
 
 
+def generate_schools(n: int) -> list[School]:
 
-    cars = []
+    fake = Faker()
+    fake.add_provider(SchoolProvider)
+
+    schools = []
     for i in range(n):
-        automatic = random.choice([True,False])
-        car = Car(
-
-            plate=fake_vehicle.license_plate(),
-            type=fake_vehicle.vehicle_make(),
-            year=random.randint(min_year, max_year),
-            automatic=automatic
+        school = School(
+            school_id= i+1,
+            name = fake.school_name(),
+            district = fake.school_district(),
+            school_level= fake.school_level()
         )
+        schools.append(school)
+    return schools
 
+def generate_passport(n: int,ervenyes: int = 5):
+    fake = Faker()
 
-        cars.append(car)
-    return cars
-
-def generate_companies(n: int, lang: str = "hu_HU") -> list[Company]:
-    fake_company = Faker(lang)
-    companies = []
-
+    passports = []
     for i in range(n):
-        company = Company(
-            name=fake_company.company(),
-            industry=fake_company.job(),
+        validity = fake.date_between(start_date='today', end_date='+10y')
+        valid_from= validity - relativedelta(years=ervenyes)
+        pp = Passport(
+            passport_number= fake.passport_number(),
+            validity= str(validity),
+            valid_from= str(valid_from)
         )
-        companies.append(company)
-    return companies
+        passports.append(pp)
+    return passports
+
+def generate_person(n: int, passports: list[Passport],schools: list[School], lang = "hu_HU"):
+    fake = Faker(lang)
+
+    persons = []
+    for i in range(n):
+
+        school_numb = random.choice(schools).school_id
+        bday = fake.date_of_birth(minimum_age=18,maximum_age=25)
+        id = passports[i].passport_number
+
+        person = Person(
+            id_number = id,
+            name = fake.name(),
+            birth_date = str(bday),
+            school_number = school_numb
+        )
+        persons.append(person)
+    return persons
 
 if __name__ == '__main__':
-    cars = generate_cars(5)
-    company = generate_companies(5)
-    for v in cars:
-        print(v)
+    n = 5
+    sc = generate_schools(n)
+    passp = generate_passport(n)
+    pers = generate_person(n,passp,sc)
+
+
+    for i in range(n):
+        print(sc[i])
     print()
-    for v in company:
-        print(v)
-
-
+    for i in range(n):
+        print(passp[i])
+        print(pers[i])
+        print()
--- data/model_classes.py
+++ data/model_classes.py
@@ -11,66 +11,89 @@ class School:
         self.school_id = school_id
 
     def __str__(self) -> str:
-        return "{name} ({district}, {school_level} : {school_id})".format(
-            name=self.name,
-            district=self.district,
-            school_level=self.school_level,
-            school_id=self.school_id
-        )
+        return f"*school* ID:{self.school_id} [{self.name}] {self.school_level} ({self.district})"
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, school_id) and self.plate == o.plate
+        return isinstance(other, School) and self.school_id == other.school_id
 
     def __hash__(self) -> int:
-        return self.plate.__hash__()
+        return self.school_id.__hash__()
 
     def __lt__(self, other):
-        if not isinstance(other, Car):
+        if not isinstance(other, School):
             return NotImplemented
-        return self.plate < other.plate
+        return self.school_id < other.school_id
 
 class Passport:
     passport_number: str
     validity: str
     valid_from: str
 
-    def __init__(self, name: str, industry: str) -> None:
-        self.name = name
-        self.industry = industry
+    def __init__(self, passport_number: str ,validity: str ,valid_from: str) -> None:
+        self.passport_number = passport_number
+        self.validity = validity
+        self.valid_from = valid_from
 
     def __str__(self) -> str:
-        return f"{self.name} ({self.industry})"
+        return f"*passport* ID:{self.passport_number}: Validity: {self.valid_from} - {self.validity}"
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, Company) and self.name == other.name
+        return isinstance(other, Passport) and self.passport_number == other.passport_number
 
     def __hash__(self) -> int:
-        return hash(self.name)
+        return hash(self.passport_number)
 
     def __lt__(self, other: object) -> bool:
-        if not isinstance(other, Company):
+        if not isinstance(other, Passport):
             return NotImplemented
-        return self.name < other.name
+        return self.passport_number < other.passport_number
 
 class Person:
     id_number: str
     name: str
     birth_date: str
+    school_number: int
 
-    def __init__(self, name: str, industry: str) -> None:
+    def __init__(self,id_number: str ,name: str ,birth_date: str ,school_number: int ) -> None:
+        self.id_number = id_number
         self.name = name
-        self.industry = industry
+        self.birth_date = birth_date
+        self.school_number = school_number
 
     def __str__(self) -> str:
-        return f"{self.name} ({self.industry})"
+        return f"*person* ID:{self.id_number} : {self.name} [{self.birth_date}] School_ID: {self.school_number}"
 
     def __eq__(self, other: object) -> bool:
-        return isinstance(other, Company) and self.name == other.name
+        return isinstance(other, Person) and self.name == other.name
 
     def __hash__(self) -> int:
         return hash(self.name)
 
     def __lt__(self, other: object) -> bool:
-        if not isinstance(other, Company):
+        if not isinstance(other, Person):
             return NotImplemented
         return self.name < other.name
+
+if __name__ == '__main__':
+    sc = School(
+        name="SZUAI",
+        school_level="alt isk",
+        school_id=2343,#int
+        district="Szobi ut"
+    )
+    passp = Passport(
+        passport_number="123123123",
+        validity="2020-12-12",
+        valid_from="2010-12-12"
+    )
+
+    pers = Person(
+        id_number=passp.passport_number,
+        name ="Kiss Zoltán",
+        birth_date="2002-03-15",
+        school_number=sc.school_id
+    )
+
+    print(sc)
+    print(passp)
+    print(pers)

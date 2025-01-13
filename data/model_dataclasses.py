d6ac52c6b582e581ba9e9c25d5ad0f9e5e70a087
czupertibor <czupertibor@gmail.com>
2025-01-13 21:52:01 +0100

    i hope its done#2

--- data/model_dataclasses.py
+++ data/model_dataclasses.py
@@ -3,20 +3,20 @@ from dataclasses import dataclass, field
 @dataclass(unsafe_hash=True)
 class School:
     school_id: int = field(hash=True)
-    name: str = field(compare=False)
-    district: str = field(compare=True)
-    school_level: str = field(compare=True)
+    name: str = field(hash=True)
+    district: str
+    school_level: str
     students: int = field(compare=True)
 
 @dataclass(unsafe_hash=True)
 class Passport:
     passport_number: str = field(hash=True)
-    validity: str = field(compare=True)
+    validity: str
     valid_from: str
 
 @dataclass(unsafe_hash=True)
 class Person:
     id_number: str = field(hash=True)
     name: str
-    birth_date: str = field(compare=True)
-    school_number: int
+    birth_date: str
+    school_number: int = field(compare=True)
